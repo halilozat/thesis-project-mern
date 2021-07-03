9fc2581d75fc294d1f9e62c3cf3694c46d8f45b7
halilozat <halilozat@hotmail.com>
2021-07-03 13:43:42 +0300

    Post CRUD operations

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,47 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+//create a post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
-router.get("/", (req,res) => {
-    res.send("aaa")
-})
-
-module.exports = router
+module.exports = router;
