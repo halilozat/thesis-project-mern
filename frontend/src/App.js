f336caf4f2ba4e437c02c9407eec0e3adf560712
halilozat <halilozat@hotmail.com>
2021-07-05 20:54:07 +0300

    Login page designed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import Navi from './layouts/Navi';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-         <Navi/>
+      <Navi />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/profile" exact component={Profile} />
+      </Switch>
     </div>
   );
 }
