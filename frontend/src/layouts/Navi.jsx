b8f26fbbc6ea78e522b72c0e7afc930c118da415
halilozat <halilozat@hotmail.com>
2021-07-05 21:33:42 +0300

    Register page designed

--- frontend/src/layouts/Navi.jsx
+++ frontend/src/layouts/Navi.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Menu, Segment, Button, Input } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
 export default function Navi() {
     return (
         <div>
             <Segment inverted >
                 <Menu inverted pointing primary fixed >
-                    <Menu.Item name='home'/>
-                    <Menu.Item name='messages'/>
-                    <Menu.Item name='friends'/>
-                    <Menu.Item position='right'>
-                        <Button secondary>Register</Button>
-                        <Button secondary>Login</Button>
-                    </Menu.Item>
+
+                    <Menu.Item><Link to="/home">Home</Link></Menu.Item>
+                    <Menu.Item><Link to="/profile">Profile</Link></Menu.Item>
+
+                    <Menu.Item position='right'><Link to="/login">Login</Link></Menu.Item>
+                    <Menu.Item><Link to="/register">Register</Link></Menu.Item>
                     
                     <Menu.Item>
                         <Input className='icon' icon='search' placeholder='Search...' />
                     </Menu.Item>
-                    
+
                 </Menu>
             </Segment>
         </div>
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -5,19 +5,20 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" >
         Your Website
       </Link>{' '}
       {new Date().getFullYear()}
@@ -70,7 +71,7 @@ export default function SignInSide() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Giriş Yap
           </Typography>
           <form className={classes.form} noValidate>
             <TextField
@@ -95,28 +96,25 @@ export default function SignInSide() {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
+            
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              color="primary"
+              color="secondary"
               className={classes.submit}
             >
-              Sign In
+              Giriş Yap
             </Button>
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
-                  Forgot password?
+                  Şifremi Unuttum
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                <Link to={"/register"}>
+                  {"Hesabınız yok mu? Kaydol"}
                 </Link>
               </Grid>
             </Grid>
