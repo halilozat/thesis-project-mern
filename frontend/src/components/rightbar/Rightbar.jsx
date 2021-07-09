699efe0e8873e55281d3404a99b0b4a201503c7f
halilozat <halilozat@hotmail.com>
2021-07-09 12:01:30 +0300

    Added rightbar component V1.0

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -2,8 +2,20 @@ import "./rightbar.css"
 
 export default function Rightbar() {
     return (
-        <div>
-            rightbar
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="assets/gift.png" alt="" />
+                    <span className="birthdayText">
+                        <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+                    </span>
+                </div>
+                <img className="rightbarAd" src="assets/ad.png" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    
+                </ul>
+            </div>
         </div>
     )
 }
