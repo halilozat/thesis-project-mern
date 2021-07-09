182269e8c927a92d140cdaf3dadfc662e61de2b1
halilozat <halilozat@hotmail.com>
2021-07-09 15:28:26 +0300

    Added categories component V1.0 and weekly component V1.0

---
+++ frontend/src/components/categories/Categories.jsx
@@ -0,0 +1,26 @@
+import './categories.css'
+
+export default function Categories() {
+    return (
+        <div className="weekly">
+                    <h4 className="rightbarTitle">Kategoriler</h4>
+                    <div className="birthdayContainer">
+                        <span className="birthdayText">
+                            <button className="secilen"></button>
+                        </span>
+                    </div>
+                    <br className="sidebarHr" />
+                    <div className="birthdayContainer">
+                        <span className="birthdayText">
+                            <button className="secilen"></button>
+                        </span>
+                    </div>
+                    <br className="sidebarHr" />
+                    <div className="birthdayContainer">
+                        <span className="birthdayText">
+                            <button className="secilen"></button>
+                        </span>
+                    </div>
+                </div>
+    )
+}
--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -2,58 +2,16 @@ import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 import { MovieIcon, LocalMovies, MenuBook } from '@material-ui/icons/Movie';
+import Weekly from "../weekly/Weekly";
+import Categories from "../categories/Categories";
 
 export default function Rightbar({ profile }) {
     const HomeRightbar = () => {
         return (
             <>
-                <div className="weekly">
-                    <h4 className="rightbarTitle">Haftanın Güzelleri</h4>
-                    <div className="birthdayContainer">
-                        <img className="birthdayImg" src="assets/filmm.jpg" alt="" />
-                        <span className="birthdayText">
-                            <span className="book">Haftanin Filmi </span>
-                            <button className="secilen">Esaretin Bedeli</button>
-                        </span>
-                    </div>
-                    <br className="sidebarHr" />
-                    <div className="birthdayContainer">
-                        <img className="birthdayImg" src="assets/dizi.png" alt="" />
-                        <span className="birthdayText">
-                            <span className="book">Haftanin Dizisi </span>
-                            <button className="secilen">Loki</button>
-                        </span>
-                    </div>
-                    <br className="sidebarHr" />
-                    <div className="birthdayContainer">
-                        <img className="birthdayImg" src="assets/book.png" alt="" />
-                        <span className="birthdayText">
-                            <span className="book">Haftanin Kitabi </span>
-                            <button className="secilen">Suç ve Ceza</button>
-                        </span>
-                    </div>
-                </div>
+                <Weekly/>
 
-                <div className="weekly">
-                    <h4 className="rightbarTitle">Kategoriler</h4>
-                    <div className="birthdayContainer">
-                        <span className="birthdayText">
-                            <button className="secilen"></button>
-                        </span>
-                    </div>
-                    <br className="sidebarHr" />
-                    <div className="birthdayContainer">
-                        <span className="birthdayText">
-                            <button className="secilen"></button>
-                        </span>
-                    </div>
-                    <br className="sidebarHr" />
-                    <div className="birthdayContainer">
-                        <span className="birthdayText">
-                            <button className="secilen"></button>
-                        </span>
-                    </div>
-                </div>
+                <Categories/>
 
                 <img className="rightbarAd" src="assets/manzara.jpg" alt="" />
 
