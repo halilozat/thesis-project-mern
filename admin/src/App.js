a2dcc5cc62dddab1e0c4c7502f019e6e1896a420
halilozat <halilozat@hotmail.com>
2021-08-02 13:37:12 +0300

    Added topbar and sidebar components

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,9 +1,16 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+      </div>
+    </Router>
   );
 }
 
