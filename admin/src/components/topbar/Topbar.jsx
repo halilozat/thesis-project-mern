205702ebb4882562aded6dd3130bd4c8eb916f91
halilozat <halilozat@hotmail.com>
2021-08-02 13:56:45 +0300

    Added react admin template

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -3,27 +3,27 @@ import "./topbar.css";
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 
 export default function Topbar() {
-    return (
-        <div className="topbar">
-            <div className="topbarWrapper">
-                <div className="topLeft">
-                    <span className="logo">Thesis Admin</span>
-                </div>
-                <div className="topRight">
-                    <div className="topbarIconContainer">
-                        <NotificationsNone />
-                        <span className="topIconBadge">2</span>
-                    </div>
-                    <div className="topbarIconContainer">
-                        <Language />
-                        <span className="topIconBadge">2</span>
-                    </div>
-                    <div className="topbarIconContainer">
-                        <Settings />
-                    </div>
-                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
-                </div>
-            </div>
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">lamaadmin</span>
         </div>
-    )
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
 }
